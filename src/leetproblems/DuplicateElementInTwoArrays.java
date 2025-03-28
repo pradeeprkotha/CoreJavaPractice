e0a029ef7bbd7282a740ddc64aa98e56d4f9bbd3
Pradeep Kotha <pradeeprkotha@gmail.com>
2025-03-28 12:16:55 -0400

    Latest code added

--- src/leetproblems/DuplicateElementInTwoArrays.java
+++ src/leetproblems/DuplicateElementInTwoArrays.java
@@ -10,7 +10,20 @@ import java.util.stream.Collectors;
 public class DuplicateElementInTwoArrays {
     public static void main(String[] args) {
         // distint and sorted arrays
-        int[] arr1 = {1,3,4,5,7};
+        int[] arr1 = {1,3,4,5,1,7};
+
+        for(int i =0; i < arr1.length; i++){
+            for (int j = i+1; j < arr1.length; j++){
+                if(arr1[i] == arr1[j]){
+                    System.out.println("Duplicate element is: "+arr1[i]);
+                    break;
+                }
+            }
+        }
+
+
+
+
         int[] arr2 = {2,3,4,6};
 
         int loop=0;
@@ -43,5 +56,20 @@ public class DuplicateElementInTwoArrays {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         System.out.println(map2);
+
+        int[] arr9 = {9,6,1,2,4,6,7}; // Sorting
+
+        for(int i =0; i < arr9.length; i++){
+            for (int j = i+1; j < arr9.length; j++){
+                if(arr9[i] > arr9[j]){
+                    int temp = arr9[i];
+                    arr9[i] = arr9[j];
+                    arr9[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr9));
+
     }
 }
